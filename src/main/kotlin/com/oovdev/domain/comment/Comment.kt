66fbbe743f3df9f62ae9d88e5d4ad21e1e6142ec
oovdev <oovdev@gmail.com>
2025-01-30 08:32:14 +0900

    feat: domain modelling and relationship setting

--- src/main/kotlin/com/oovdev/domain/comment/Comment.kt
+++ src/main/kotlin/com/oovdev/domain/comment/Comment.kt
@@ -1,14 +1,21 @@
 package com.oovdev.domain.comment
 
 import com.oovdev.domain.AuditingEntity
-import javax.persistence.Entity
-import javax.persistence.Table
+import com.oovdev.domain.post.Post
+import javax.persistence.*
 
 @Table(name = "Comment")
 @Entity
 class Comment(
-
+    content: String,
+    post: Post,
 ): AuditingEntity() {
 
+    @Column(name = "content", nullable =  false)
+    var content: String = content
+        protected set
 
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Post::class)
+    var post: Post = post
+        protected set
 }
--- src/main/kotlin/com/oovdev/domain/post/Post.kt
+++ src/main/kotlin/com/oovdev/domain/post/Post.kt
@@ -1,6 +1,7 @@
 package com.oovdev.domain.post
 
 import com.oovdev.domain.AuditingEntity
+import com.oovdev.domain.member.Member
 import javax.persistence.*
 
 @Table(name = "Post")
@@ -8,6 +9,7 @@ import javax.persistence.*
 class Post(
     title: String,
     content: String,
+    member: Member,
 ) : AuditingEntity() {
 
     @Column(name = "title", nullable = false)
@@ -16,5 +18,9 @@ class Post(
 
     @Column(name = "content", nullable = true)
     var content: String = content
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Member::class)
+    var member: Member = member
         protected set
 }
